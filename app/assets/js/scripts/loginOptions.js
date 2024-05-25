4f49e7a10a730bc0792808964bae9b0e26058d25
Redllamaaa <masterhanlon867@gmail.com>
2024-05-25 20:56:47 +0100

    Removed Mojang Accounts
    Since Mojang Auth Servers went down, this is pointless

--- app/assets/js/scripts/loginOptions.js
+++ app/assets/js/scripts/loginOptions.js
@@ -1,6 +1,5 @@
 const loginOptionsCancelContainer = document.getElementById('loginOptionCancelContainer')
 const loginOptionMicrosoft = document.getElementById('loginOptionMicrosoft')
-const loginOptionMojang = document.getElementById('loginOptionMojang')
 const loginOptionsCancelButton = document.getElementById('loginOptionCancelButton')
 
 let loginOptionsCancellable = false
@@ -25,26 +24,5 @@ loginOptionMicrosoft.onclick = (e) => {
             loginOptionsViewOnLoginSuccess,
             loginOptionsViewOnLoginCancel
         )
-    })
-}
-
-loginOptionMojang.onclick = (e) => {
-    switchView(getCurrentView(), VIEWS.login, 500, 500, () => {
-        loginViewOnSuccess = loginOptionsViewOnLoginSuccess
-        loginViewOnCancel = loginOptionsViewOnLoginCancel
-        loginCancelEnabled(true)
-    })
-}
-
-loginOptionsCancelButton.onclick = (e) => {
-    switchView(getCurrentView(), loginOptionsViewOnCancel, 500, 500, () => {
-        // Clear login values (Mojang login)
-        // No cleanup needed for Microsoft.
-        loginUsername.value = ''
-        loginPassword.value = ''
-        if(loginOptionsViewCancelHandler != null){
-            loginOptionsViewCancelHandler()
-            loginOptionsViewCancelHandler = null
-        }
     })
 }
