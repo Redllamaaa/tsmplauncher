f9eb91340686941d1fea92ae47f5c3a1ae25ada9
Redllamaaa <masterhanlon867@gmail.com>
2024-05-25 09:03:07 +0100

    Removed News Menu

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -89,11 +89,6 @@ const DEFAULT_CONFIG = {
             dataDirectory: dataPath
         }
     },
-    newsCache: {
-        date: null,
-        content: null,
-        dismissed: false
-    },
     clientToken: null,
     selectedServer: null, // Resolved
     selectedAccount: null,
@@ -206,34 +201,6 @@ exports.getTempNativeFolder = function(){
 }
 
 // System Settings (Unconfigurable on UI)
-
-/**
- * Retrieve the news cache to determine
- * whether or not there is newer news.
- * 
- * @returns {Object} The news cache object.
- */
-exports.getNewsCache = function(){
-    return config.newsCache
-}
-
-/**
- * Set the new news cache object.
- * 
- * @param {Object} newsCache The new news cache object.
- */
-exports.setNewsCache = function(newsCache){
-    config.newsCache = newsCache
-}
-
-/**
- * Set whether or not the news has been dismissed (checked)
- * 
- * @param {boolean} dismissed Whether or not the news has been dismissed (checked).
- */
-exports.setNewsCacheDismissed = function(dismissed){
-    config.newsCache.dismissed = dismissed
-}
 
 /**
  * Retrieve the common directory for shared
