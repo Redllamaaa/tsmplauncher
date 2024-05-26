c5e77e75612a7dd004f96fc5ef74c6bff3824605
Redllamaaa <masterhanlon867@gmail.com>
2024-05-26 08:14:13 +0100

    Updated github Links

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1435,7 +1435,7 @@ function populateAboutVersionInformation(){
  */
 function populateReleaseNotes(){
     $.ajax({
-        url: 'https://github.com/dscalzi/HeliosLauncher/releases.atom',
+        url: 'https://github.com/Redllamaaa/tsmplauncher/releases.atom',
         success: (data) => {
             const version = 'v' + remote.app.getVersion()
             const entries = $(data).find('entry')
