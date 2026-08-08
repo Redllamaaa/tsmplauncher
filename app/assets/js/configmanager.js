8cca8a00c5378268f8d9c4a153c50cd21c31d18a
Redllamaaa <91138418+Redllamaaa@users.noreply.github.com>
2026-08-08 17:09:23 +0100

    feat(configmanager): New JVM Options for Java 25

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -547,7 +547,9 @@ exports.setModConfiguration = function (serverid, configuration) {
 // Java Settings
 
 function defaultJavaConfig(effectiveJavaOptions, ram) {
-  if (effectiveJavaOptions.suggestedMajor > 8) {
+  if (effectiveJavaOptions.suggestedMajor > 17) {
+    return defaultJavaConfig25(ram);
+  } else if (effectiveJavaOptions.suggestedMajor > 8) {
     return defaultJavaConfig17(ram);
   } else {
     return defaultJavaConfig8(ram);
@@ -580,6 +582,20 @@ function defaultJavaConfig17(ram) {
       "-XX:G1ReservePercent=20",
       "-XX:MaxGCPauseMillis=50",
       "-XX:G1HeapRegionSize=32M",
+    ],
+  };
+}
+
+function defaultJavaConfig25(ram) {
+  return {
+    minRAM: resolveSelectedRAM(ram),
+    maxRAM: resolveSelectedRAM(ram),
+    executable: null,
+    jvmOptions: [
+      "-XX:+UseCompactObjectHeaders",
+      "-XX:+AlwaysPreTouch",
+      "-XX:+UseStringDeduplication",
+      "-XX:+UseZGC",
     ],
   };
 }
